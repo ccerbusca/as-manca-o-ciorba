e3952e95be4b426867ce6974a36237c3d619ad47
CMihai998 <mihai3198@gmail.com>
2020-05-27 23:42:07 +0300

    Added route for my-submissions

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,12 +4,15 @@ import {SignUpComponent} from './sign-up/sign-up.component';
 import {LogInComponent} from './log-in/log-in.component';
 import {AppComponent} from './app.component';
 import {AlreadyLoggedGuardService} from './shared/auth/guards/already-logged-guard.service';
+import {MySubmissionsComponent} from './user/my-submissions/my-submissions.component';
+import {AuthGuardService} from "./shared/auth/guards/auth-guard.service";
 
 
 const routes: Routes = [
   {path: '', component: AppComponent, pathMatch: 'full'},
   {path: 'signup', component: SignUpComponent, canActivate: [AlreadyLoggedGuardService]},
-  {path: 'login', component: LogInComponent, canActivate: [AlreadyLoggedGuardService]}
+  {path: 'login', component: LogInComponent, canActivate: [AlreadyLoggedGuardService]},
+  {path: 'my-submissions', component: MySubmissionsComponent, canActivate: [AuthGuardService]}
 ];
 
 @NgModule({
